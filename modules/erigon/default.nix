291b1ef054c40efdc4ea1f17eae0047043e2cdaf
Richard Warfield <richard@litx.io>
2024-03-15 18:25:14 +0700

    missing backslash after jwtsecret in erigon

--- modules/erigon/default.nix
+++ modules/erigon/default.nix
@@ -102,7 +102,7 @@ in {
 
             in ''
               --datadir ${datadir} \
-              ${jwtsecret}
+              ${jwtsecret} \
               ${concatStringsSep " \\\n" filteredArgs} \
               ${lib.escapeShellArgs cfg.extraArgs}
             '';
